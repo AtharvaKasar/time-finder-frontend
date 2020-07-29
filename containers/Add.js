8280cd2bd7b6069f9b149eaf09fc2d1224c619f3
Atharva Kasar <19kasara@abschools.org>
2020-07-29 14:10:54 -0400

    adding text inputs

--- containers/Add.js
+++ containers/Add.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function Add() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
     return (
         <View style={styles.container}>
-            <Text>Add an event!</Text>
+            <Text>Event Name:</Text>
+            <TextInput style={styles.input} 
+                       placeholder='e.g. Lunch Meeting'
+                       onChangeText={(val) => setName(val)}/>
+
+            <Text>Event Description:</Text>
+            <TextInput style={styles.input} 
+                       placeholder='e.g. Meeting to discuss Lunch'
+                       onChangeText={(val) => setDescription(val)}/>
+
+            <Text>Name: {name}, Description: {description}</Text>
         </View>
     )
 }
@@ -16,5 +29,12 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200
+    }
   });
   
