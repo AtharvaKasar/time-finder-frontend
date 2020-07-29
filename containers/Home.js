f2326c5b6ae9eba457a1a3fe6d0a12c00eac5ecc
Atharva Kasar <19kasara@abschools.org>
2020-07-29 10:55:15 -0400

    dealing with state and card components

--- containers/Home.js
+++ containers/Home.js
@@ -1,21 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+// Home.js
+ 
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Card from '../components/Card'
-
+ 
 export default function Home() {
+ 
+    const [cards, updateCards] = useState([])
+
+    const clickHandler = () => {
+        console.log('button pressed');
+        updateCards(fetchCards);
+        console.log(cards)
+    }
+ 
+    const fetchCards = () => {
+        return [
+            {
+                title: "1",
+                calendar: 1,
+                description: "Lipsum",
+                start: "9:00 AM",
+                end: "11:00 AM"
+            },
+            {
+                title: "2",
+                calendar: 1,
+                description: "Lipsum",
+                start: "9:00 AM",
+                end: "11:00 AM"
+            },
+            {
+                title: "3",
+                calendar: 1,
+                description: "Lipsum",
+                start: "9:00 AM",
+                end: "11:00 AM"
+            },
+            {
+                title: "4",
+                calendar: 1,
+                description: "Lipsum",
+                start: "9:00 AM",
+                end: "11:00 AM"
+            },
+        ]
+    }
+ 
     return (
         <View style={styles.container}>
-            <Card
-                title="Meeting # 1"
-                calendar="Work"
-                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                start="9:00 AM"
-                end="11:00 AM"
-            />
+            {
+                cards
+ 
+                ?
+ 
+                cards.map(card => (
+                    <Card 
+                        key={card.title}
+                        title={card.title}
+                        calendar={card.calendar}
+                        description={card.description}
+                        start={card.start}
+                        end={card.end}
+                    />
+                ))
+ 
+                :
+ 
+                <Text>No Results :(</Text>
+            }
+
+            <View style={styles.buttonContainer}>
+                <Button title='add cards' onPress={clickHandler}/>
+            </View>
         </View>
     )
 }
-
+ 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -23,5 +84,7 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    buttonContainer: {
+        marginTop: 20
+    }
   });
-  
